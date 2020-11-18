6e553bdb5b5608387616deae8ce68033feac488c
Zoe Harper <zoe.writes.97@gmail.com>
2020-11-18 16:08:01 -0500

    js-basics

---
+++ index.js
@@ -0,0 +1,47 @@
+/*const greeting = 'Hello, ';
+const nameOne = 'Zoe Harper';
+const nameTwo = 'Angel';
+const nameThree = 'Teresa';
+const nameFour = 'Lawrence';
+console.log(greeting + nameOne);
+console.log(greeting + nameTwo);
+console.log(greeting + nameThree);
+console.log(greeting + nameFour); */
+
+const variableOne = 5;
+const variableTwo = 8;
+const variableThree = variableOne + variableTwo;
+const variableFour = variableThree / 4;
+console.log(variableThree); 
+console.log(variableFour);
+
+const numberSetOne = 1 + 2 + 3 + 4 + 5;
+const numberSetTwo = 6 + 7 + 8 + 9 + 10;
+const averageOne = numberSetOne/5;
+const averageTwo = numberSetTwo/5;
+console.log(averageOne);
+console.log(averageTwo);
+
+
+//const stringAndNumString = "Hello + 2+ 2+ 2+ 2+ 2+ 2+ 2+ 2+ 2" + '+';
+//console.log(stringAndNumString);
+//expected > Hello18+
+//outcome > Hello + 2+ 2+ 2+ 2+ 2+ 2+ 2+ 2+ 2+
+
+//let x, y, z;
+//x = 1200;
+//y = z;
+//z = x + y;
+//console.log(x, y, z);
+//x+y=1200+1200+y
+//outcome 1200 undefined NaN
+
+//const bondJames = 007;
+//console.log(BondJames);
+//error code
+//outcome > error code
+
+//let y = 12;
+//console.log(y + 1 + x);
+//13x
+//error x is not defined
